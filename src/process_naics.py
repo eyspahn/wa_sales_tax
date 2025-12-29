62e84a6a19dc973ccd5ebc4c27a534e111299e69
Emily Y Spahn <emilyyspahn@Goldie.local>
2025-12-28 19:59:25 -0800

    Updated processing, toml, lock files

--- src/process_naics.py
+++ src/process_naics.py
@@ -1,18 +1,30 @@
 # Let's use polars to process data ready for duckdb.
+import logging
 import polars as pl
 
-df = pl.read_excel(source='../data/NAICS/NAICS_codes_2022.xlsx')
-df = df.rename({'2022 NAICS US   Code':'code', '2022 NAICS US Title':'description'})
 
-# polars detected NAICS codes as integers
-# we have to convert to int & right-pad with 0s to create the 6 digit code
-# and drop any duplicates created
-df = df.select(
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+# Q... there's a blank row in the source data, does polars deal with this automatically?
+
+for year in [2012, 2017, 2022]:
+    fpath = f'../data/NAICS/2-6_digit_{year}_Codes.xlsx'
+    if year == 2012:
+        fpath = f'../data/NAICS/2-6_digit_{year}_Codes.xls'    
+
+    logger.info(f'Working on year {year}')
+    df = pl.read_excel(source=fpath)
+    df = df.rename({f'{year} NAICS US   Code':'code', f'{year} NAICS US Title':'description'})
+
+    df = df.select(
         pl.col('code').cast(pl.String).str.pad_end(6,'0'),
         pl.col('description').str.strip_chars(),
+        pl.lit(year).alias('year'),
     ).unique(maintain_order=True)
 
-# went from 2125 -> 1603 rows
-df.write_parquet(file='../data/NAICS/naics_codes_2022.parquet',
+    # went from 2125 -> 1603 rows
+    fname_output = f'../data/NAICS/naics_codes_{year}.parquet'
+    df.write_parquet(file=fname_output,
                  compression='snappy')
-
+    logger.info(f"Wrote {fname_output}")
--- src/process_wa_tax.py
+++ src/process_wa_tax.py
@@ -1,53 +1,70 @@
 from pathlib import Path
+
+import duckdb
 import polars as pl
 
 
-base_path = "../data/WA_tax/"
-data_path = Path(base_path)
 
-sources = (p.name for p in data_path.rglob("*.xlsx"))
+def process_sales_tax_data(source_folder: str = "../data/WA_tax/"):
+    """
+    Reads excel files for WA state sales taxes
+    Saves to parquet
+    """
 
-for source in sources:
+    data_path = Path(source_folder)
+    sources = (p.name for p in data_path.rglob("*.xlsx"))
 
-    # first 10 or 11 lines should be skipped, but depends on file
-    # Also some have a blank line between header row & data.
-    # set the column fields & read in data only from file.
+    for source in sources:
+
+        # extract year from source name
+        year = int(source.split('.')[0][-4:])
+
+        if year in (2018, 2019, 2022, 2023, 2024):
+            skip_row_value = 10 # data starts at row 11
+        elif year in (2020, 2021):
+            skip_row_value = 11 # data starts at row 12
+        else:
+            skip_row_value = 12 # data starts at row 13
+
+        df = pl.read_excel(source=source_folder+source,
+                        has_header=False,
+                        read_options={ "skip_rows": skip_row_value})
+
+        df = df.rename(
+            { 'column_1': 'NAICS',
+            'column_2': 'tax_type',
+            'column_3': 'line_code',
+            'column_4': 'line_code_description',
+            'column_5': 'reporting_units',
+            'column_6': 'gross',
+            'column_7': 'taxable',
+            'column_8': 'taxdue' })
+
+        df = df.with_columns(pl.lit(year).alias('year'))
+
+        # There are entries with "D" to indicate there are insufficient data 
+        # to aggregate the reporting. Let's replace these & set the types.
+        cols_for_replace = [
+            'reporting_units', 'gross', 'taxable', 'taxdue'
+            ]
+        
+        df = df.with_columns(
+            pl.col(col).replace({'D':None}).alias(col) 
+            for col in cols_for_replace
+        )
+
+        # let's set NAICS code as a string, given how we'll use it
+        # Money amounts in the file are rounded to the nearest dollar (no cents)
+        df = df.cast({'NAICS': pl.String,
+                      'reporting_units': pl.Int64,
+                      'gross': pl.Int64,
+                      'taxable': pl.Int64,
+                      'taxdue': pl.Int64
+                      })
+
+        df.write_parquet(file=f'../data/WA_tax/wa_sales_tax_{year}.parquet',
+                        compression='snappy')
 
 
-
-    # extract year from source name
-    year = int(source.split('.')[0][-4:])
-
-
-    # data starts at row 13: years: 2012, 2013 - 2017
-    # data starts at row 11: 2018 - 2024
-
-    if year >= 2018:
-        skip_row_value = 10 # data starts at row 11
-    else:
-        skip_row_value = 12 # data starts at row 13
-
-    df = pl.read_excel(source=base_path+source,
-                       has_header=False,
-                       read_csv_options={ "skip_rows": skip_row_value})
-
-    # rename columns
-    df = df.rename(
-        { 'column_1': 'NAICS',
-         'column_2': 'tax_type',
-         'column_3': 'line_code',
-         'column_4': 'line_code_description',
-         'column_5': 'reporting_units',
-         'column_6': 'gross',
-         'column_7': 'taxable',
-         'column_8': 'taxdue' })
-
-    # add a column with the year to the data
-    df = df.with_columns(pl.lit(year).alias('year'))
-
-    # let's keep NAICS code a string
-    df = df.cast({'NAICS': pl.String})
-
-
-    # to do - write parquet file out
-
+if __name__=="__main__":
+    process_sales_tax_data()
